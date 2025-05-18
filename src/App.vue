2099c89b591d08c6db59b8bc98692650e109e682
DiegoPinochet <dppinochet@gmail.com>
2025-05-18 19:45:31 -0400

    💄 adds weather by date dummy card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import WeatherByHourCard from './components/weather-by-hour/WeatherByHourCard.vue'
+import WeatherByDateCard from './components/weather-by-date/WeatherByDateCard.vue'
 
 const weatherByHour = [
   {
@@ -101,10 +102,76 @@ const weatherByHour = [
     isLast: true,
   },
 ]
+
+const weatherByDate = [
+  {
+    date: 'Today',
+    maxTemperature: 24,
+    minTemperature: 18,
+    rainPercentage: 30,
+    weatherIcon: 'https://openweathermap.org/img/wn/02d@2x.png',
+    description: 'Partly cloudy with light rain',
+    isLast: false,
+  },
+  {
+    date: 'Tomorrow',
+    maxTemperature: 23,
+    minTemperature: 17,
+    rainPercentage: 45,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    description: 'Rain showers likely',
+    isLast: false,
+  },
+  {
+    date: 'Wed',
+    maxTemperature: 22,
+    minTemperature: 16,
+    rainPercentage: 60,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    description: 'Heavy rain expected',
+    isLast: false,
+  },
+  {
+    date: 'Thu',
+    maxTemperature: 21,
+    minTemperature: 15,
+    rainPercentage: 40,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    description: 'Scattered showers',
+    isLast: false,
+  },
+  {
+    date: 'Fri',
+    maxTemperature: 23,
+    minTemperature: 16,
+    rainPercentage: 20,
+    weatherIcon: 'https://openweathermap.org/img/wn/02d@2x.png',
+    description: 'Partly sunny',
+    isLast: false,
+  },
+  {
+    date: 'Sat',
+    maxTemperature: 25,
+    minTemperature: 18,
+    rainPercentage: 10,
+    weatherIcon: 'https://openweathermap.org/img/wn/01d@2x.png',
+    description: 'Sunny skies',
+    isLast: false,
+  },
+  {
+    date: 'Sun',
+    maxTemperature: 26,
+    minTemperature: 19,
+    rainPercentage: 5,
+    weatherIcon: 'https://openweathermap.org/img/wn/01d@2x.png',
+    description: 'Clear and sunny',
+    isLast: true,
+  },
+]
 </script>
 
 <template>
-  <div class="size-full p-4">
+  <div class="size-full p-4 bg-blue-100">
     <header class="mb-8">
       <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
     </header>
@@ -112,6 +179,7 @@ const weatherByHour = [
     <main class="flex justify-center items-center">
       <div class="flex flex-col gap-4 w-full max-w-xl justify-center items-center">
         <WeatherByHourCard :weather-by-hour="weatherByHour" />
+        <WeatherByDateCard :weather-by-date="weatherByDate" />
       </div>
     </main>
   </div>
---
+++ src/components/weather-by-date/WeatherByDateCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import WeatherByDate from './WeatherByDate.vue'
+
+const props = defineProps<{
+  weatherByDate: {
+    minTemperature: number
+    maxTemperature: number
+    weatherIcon: string
+    date: string
+    description: string
+    isLast?: boolean
+  }[]
+}>()
+</script>
+
+<template>
+  <Card class="w-full">
+    <CardHeader>
+      <CardTitle>Next 5 days</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <div class="flex flex-col items-center w-full">
+        <WeatherByDate
+          v-for="date in props.weatherByDate"
+          :key="date.date"
+          :minTemperature="date.minTemperature"
+          :maxTemperature="date.maxTemperature"
+          :weatherIcon="date.weatherIcon"
+          :date="date.date"
+          :description="date.description"
+          :is-last="date.isLast"
+        />
+      </div>
+    </CardContent>
+  </Card>
+</template>
