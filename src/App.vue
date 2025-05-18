64d20dd51ba8fce7ca78f75c78d83be9545b7d8d
DiegoPinochet <dppinochet@gmail.com>
2025-05-18 19:36:17 -0400

    💄 adds dummy weather by hour card

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,118 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WeatherByHourCard from './components/weather-by-hour/WeatherByHourCard.vue'
+
+const weatherByHour = [
+  {
+    temperature: 22,
+    rainPercentage: 10,
+    weatherIcon: 'https://openweathermap.org/img/wn/01d@2x.png',
+    hour: 'Now',
+    isLast: false,
+  },
+  {
+    temperature: 23,
+    rainPercentage: 15,
+    weatherIcon: 'https://openweathermap.org/img/wn/01d@2x.png',
+    hour: '13:00',
+    isLast: false,
+  },
+  {
+    temperature: 24,
+    rainPercentage: 20,
+    weatherIcon: 'https://openweathermap.org/img/wn/02d@2x.png',
+    hour: '14:00',
+    isLast: false,
+  },
+  {
+    temperature: 23,
+    rainPercentage: 25,
+    weatherIcon: 'https://openweathermap.org/img/wn/02d@2x.png',
+    hour: '15:00',
+    isLast: false,
+  },
+  {
+    temperature: 22,
+    rainPercentage: 30,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    hour: '16:00',
+    isLast: false,
+  },
+  {
+    temperature: 21,
+    rainPercentage: 40,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    hour: '17:00',
+    isLast: false,
+  },
+  {
+    temperature: 20,
+    rainPercentage: 35,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    hour: '18:00',
+    isLast: true,
+  },
+  {
+    temperature: 22,
+    rainPercentage: 10,
+    weatherIcon: 'https://openweathermap.org/img/wn/01d@2x.png',
+    hour: 'Now',
+    isLast: false,
+  },
+  {
+    temperature: 23,
+    rainPercentage: 15,
+    weatherIcon: 'https://openweathermap.org/img/wn/01d@2x.png',
+    hour: '13:00',
+    isLast: false,
+  },
+  {
+    temperature: 24,
+    rainPercentage: 20,
+    weatherIcon: 'https://openweathermap.org/img/wn/02d@2x.png',
+    hour: '14:00',
+    isLast: false,
+  },
+  {
+    temperature: 23,
+    rainPercentage: 25,
+    weatherIcon: 'https://openweathermap.org/img/wn/02d@2x.png',
+    hour: '15:00',
+    isLast: false,
+  },
+  {
+    temperature: 22,
+    rainPercentage: 30,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    hour: '16:00',
+    isLast: false,
+  },
+  {
+    temperature: 21,
+    rainPercentage: 40,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    hour: '17:00',
+    isLast: false,
+  },
+  {
+    temperature: 20,
+    rainPercentage: 35,
+    weatherIcon: 'https://openweathermap.org/img/wn/10d@2x.png',
+    hour: '18:00',
+    isLast: true,
+  },
+]
+</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="size-full p-4">
+    <header class="mb-8">
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    </header>
 
-    <div class="bg-red-500 p-8">Hello</div>
-  </header>
-
-  <main>Hello</main>
+    <main class="flex justify-center items-center">
+      <div class="flex flex-col gap-4 w-full max-w-xl justify-center items-center">
+        <WeatherByHourCard :weather-by-hour="weatherByHour" />
+      </div>
+    </main>
+  </div>
 </template>
---
+++ src/components/weather-by-hour/WeatherByHour.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { P } from '@/components/ui/typography'
+import { Separator } from '@/components/ui/separator'
+
+defineProps<{
+  temperature: number
+  rainPercentage: number
+  weatherIcon: string
+  hour: string
+  isLast?: boolean
+}>()
+</script>
+
+<template>
+  <div class="flex items-center">
+    <div class="flex flex-col gap-2 p-2 items-center">
+      <H4>{{ temperature }}</H4>
+      <P class="text-blue-300">{{ rainPercentage }}%</P>
+      <img :src="weatherIcon" alt="weather icon" class="size-4" />
+      <P class="text-muted-foreground">{{ hour }}</P>
+    </div>
+    <Separator v-if="!isLast" orientation="vertical" class="mx-2 !h-20" />
+  </div>
+</template>
